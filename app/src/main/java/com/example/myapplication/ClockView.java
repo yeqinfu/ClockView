7cbe72eb9141ece135a20be07b9d4c07342ccca3
yeqinfu <990761790>
2019-05-24 13:48:40 +0800

    动画

--- app/src/main/java/com/example/myapplication/ClockView.java
+++ app/src/main/java/com/example/myapplication/ClockView.java
@@ -1,16 +1,23 @@
 package com.example.myapplication;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.Rect;
-import android.support.annotation.NonNull;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
+import android.view.animation.LinearInterpolator;
+
+import java.util.Calendar;
 
 /**
+ * 年不做处理，我不相信能用一年
  * Created by yeqinfu on 23/05/2019
  */
 public class ClockView extends View {
@@ -27,6 +34,7 @@ public class ClockView extends View {
     public ClockView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         init();
+
     }
 
     int mWidth = 0;
@@ -52,16 +60,19 @@ public class ClockView extends View {
     private String second = "五十九秒";
     private float radiusOffset = 10;
     private Paint textPaint = null;
+    private Paint textPaintGray = null;
+
     /**
      * sp 转 px
      *
      * @param spValue {@code spValue}
      * @return {@code pxValue}
      */
-    public  int sp2px( float spValue) {
+    public int sp2px(float spValue) {
         final float fontScale = getResources().getDisplayMetrics().scaledDensity;
         return (int) (spValue * fontScale + 0.5f);
     }
+
     public Paint getTextPaint() {
         if (textPaint == null) {
             textPaint = new Paint();
@@ -72,37 +83,75 @@ public class ClockView extends View {
         }
         return textPaint;
     }
-    private float getTextLength(Paint paint,String text){
+
+    public Paint getTextPaintGray() {
+        if (textPaintGray == null) {
+            textPaintGray = new Paint();
+            textPaintGray.setAntiAlias(true);
+            textPaintGray.setColor(Color.GRAY);
+            textPaintGray.setTextSize(sp2px(6));
+            textPaintGray.setStyle(Paint.Style.FILL);
+        }
+        return textPaint;
+    }
+
+    private float getTextLength(Paint paint, String text) {
         return paint.measureText(text);
     }
-    private float getTextLength(String text){
-        return getTextLength(getTextPaint(),text);
+
+    private float getTextLength(String text) {
+        return getTextLength(getTextPaint(), text);
     }
+
     /***
      * 各种半径
      */
-    private float yearRadius=0;
-    private float monthRadius=0;
-    private float dayRadius=0;
-    private float dayHalfRadius=0;
-    private float hourRadius=0;
-    private float minuteRadius=0;
-    private float secondRadius=0;
-    private void init(){
-        yearRadius=getTextLength(year)/2+radiusOffset;
-        monthRadius=yearRadius+radiusOffset+getTextLength(month);
-        dayRadius=monthRadius+radiusOffset+getTextLength(day);
-        dayHalfRadius=dayRadius+radiusOffset+getTextLength(dayhelf);
-        hourRadius=dayHalfRadius+radiusOffset+getTextLength(hour);
-        minuteRadius=hourRadius+radiusOffset+getTextLength(minute);
-        secondRadius=minuteRadius+radiusOffset+getTextLength(second);
-        commonTextHeight=-getTextPaint().ascent() + getTextPaint().descent();
+    private float yearRadius = 0;
+    private float monthRadius = 0;
+    private float dayRadius = 0;
+    private float dayHalfRadius = 0;
+    private float hourRadius = 0;
+    private float minuteRadius = 0;
+    private float secondRadius = 0;
+
+    private void init() {
+        yearRadius = getTextLength(year) / 2 + radiusOffset;
+        monthRadius = yearRadius + radiusOffset + getTextLength(month);
+        dayRadius = monthRadius + radiusOffset + getTextLength(day);
+        dayHalfRadius = dayRadius + radiusOffset + getTextLength(dayhelf);
+        hourRadius = dayHalfRadius + radiusOffset + getTextLength(hour);
+        minuteRadius = hourRadius + radiusOffset + getTextLength(minute);
+        secondRadius = minuteRadius + radiusOffset + getTextLength(second);
+        commonTextHeight = -getTextPaint().ascent() + getTextPaint().descent();
+       // startAnimator();
+      //  setDateAndStart(null);
+    }
+    public void setDateAndStart(Calendar calendar){
+        if (calendar==null){
+            calendar=Calendar.getInstance();
+        }
+        monthRepeat=calendar.get(Calendar.MONTH);
+        dayRepeat=calendar.get(Calendar.DAY_OF_MONTH);
+        dayHalfRepeat=calendar.get(Calendar.HOUR_OF_DAY)>12?1:0;
+        if (calendar.get(Calendar.HOUR_OF_DAY)>12){
+            hourRepeat=calendar.get(Calendar.HOUR_OF_DAY)-12;
+        }else{
+            hourRepeat=calendar.get(Calendar.HOUR_OF_DAY);
+        }
+        minuteRepeat=calendar.get(Calendar.MINUTE);
+        secondRepeat=calendar.get(Calendar.SECOND);
+        startAnimator();
+
+
+
     }
 
     /**
      * 因为画笔只有一根，高度肯定是只有一个
      */
-    private float commonTextHeight=0;
+    private float commonTextHeight = 0;
+    /*判断是否是动画状态*/
+    private boolean isMove = false;
 
     @Override
     protected void onDraw(Canvas canvas) {
@@ -111,87 +160,93 @@ public class ClockView extends View {
         canvas.translate(mWidth / 2, mHeight / 2);
 
         /**画中心*/
-        drawTextOnMiddle(canvas,year,0,0);
+        drawTextOnMiddle(canvas, year, 0, 0);
         /**画月份*/
         double initAngle = 0;
         canvas.save();
+        canvas.rotate(monthRepeat * (360.0f / 12) + (float) monInitAngle);
 
-        for (int i=0;i<12;i++){
-            Polar polar=new Polar(monthRadius,initAngle);
-            drawTextToPointLeft(canvas,polar.toPoint(),CommonNumber.getMonths()[i]);
-            canvas.rotate(360/12);
+        for (int i = 0; i < 12; i++) {
+            Polar polar = new Polar(monthRadius, initAngle);
+            drawTextToPointLeft(canvas, polar.toPoint(), CommonNumber.getMonths()[i], getTextPaint());
+            canvas.rotate(-360 / 12);
         }
         canvas.restore();
         canvas.save();
         /**画日期*/
-        String[] days=CommonNumber.getDays(null);
-        initAngle=0;
-        for (int i=0;i<days.length;i++){
-            Polar polar=new Polar(dayRadius,initAngle);
-            drawTextToPointLeft(canvas,polar.toPoint(),days[i]);
-            canvas.rotate(360.0f/(days.length));
+        String[] days = CommonNumber.getDays(null);
+        initAngle = 0;
+        canvas.rotate(dayRepeat * (360.0f / days.length) + (float) dayInitAngle);
+        for (int i = 0; i < days.length; i++) {
+            Polar polar = new Polar(dayRadius, initAngle);
+            drawTextToPointLeft(canvas, polar.toPoint(), days[i], getTextPaint());
+            canvas.rotate(-360.0f / (days.length));
         }
         canvas.restore();
         /**画上午下午*/
         canvas.save();
-        initAngle=0;
-        Polar polar1=new Polar(dayHalfRadius,initAngle);
-        drawTextToPointLeft(canvas,polar1.toPoint(),"上午");
+        initAngle = 0;
+        canvas.rotate(dayHalfRepeat * 180 + (float) dayHalfInitAngle);
+        Polar polar1 = new Polar(dayHalfRadius, initAngle);
+        drawTextToPointLeft(canvas, polar1.toPoint(), "上午", getTextPaint());
         canvas.rotate(180);
-        drawTextToPointLeft(canvas,polar1.toPoint(),"下午");
+        drawTextToPointLeft(canvas, polar1.toPoint(), "下午", getTextPaint());
         canvas.restore();
         /**画小时*/
         canvas.save();
-        initAngle=0;
-        String[] hours=CommonNumber.getHours();
-        for (int i=0;i<12;i++){
-            Polar polar=new Polar(hourRadius,initAngle);
-            drawTextToPointLeft(canvas,polar.toPoint(),hours[i]);
-            canvas.rotate(360.0f/12);
+        initAngle = 0;
+        String[] hours = CommonNumber.getHours();
+        canvas.rotate(hourRepeat * 30 + (float) hourInitAngle);
+        for (int i = 0; i < 12; i++) {
+            Polar polar = new Polar(hourRadius, initAngle);
+            drawTextToPointLeft(canvas, polar.toPoint(), hours[i], getTextPaint());
+            canvas.rotate(-360.0f / 12);
         }
         canvas.restore();
         /**画分钟*/
         canvas.save();
-        initAngle=0;
-        String[] minutes=CommonNumber.getMinutes();
-        for (int i=0;i<60;i++){
-            Polar polar=new Polar(minuteRadius,initAngle);
-            drawTextToPointLeft(canvas,polar.toPoint(),minutes[i]);
-            canvas.rotate(360.0f/60);
+        initAngle = 0;
+        String[] minutes = CommonNumber.getMinutes();
+        canvas.rotate(minuteRepeat * 6 + (float) minuteInitAngle);
+        for (int i = 0; i < 60; i++) {
+            Polar polar = new Polar(minuteRadius, initAngle);
+            drawTextToPointLeft(canvas, polar.toPoint(), minutes[i], getTextPaint());
+            canvas.rotate(-360.0f / 60);
         }
         canvas.restore();
 
         /**画秒*/
         canvas.save();
-        initAngle=0;
-        String[] seconds=CommonNumber.getSeconds();
-        for (int i=0;i<60;i++){
-            Polar polar=new Polar(secondRadius,initAngle);
-            drawTextToPointLeft(canvas,polar.toPoint(),seconds[i]);
-            canvas.rotate(360.0f/60);
+        initAngle = 0;
+        String[] seconds = CommonNumber.getSeconds();
+        /*移动和禁止状态画笔颜色不一样*/
+        Paint p = isMove ? getTextPaintGray() : getTextPaint();
+        canvas.rotate(secondRepeat * 6 + (float) secondInitAngle);
+        Log.d("yeqinfu", "---------->" + secondRepeat + "==" + secondInitAngle);
+        for (int i = 0; i < 60; i++) {
+            Polar polar = new Polar(secondRadius, initAngle);
+            drawTextToPointLeft(canvas, polar.toPoint(), seconds[i], p);
+            canvas.rotate((float) -(360.0f / 60));
         }
         canvas.restore();
 
 
-
-
-
-
-
     }
 
-    private void drawTextToPointLeft(Canvas canvas,Point toPoint, String targetText) {
+
+    private void drawTextToPointLeft(Canvas canvas, Point toPoint, String targetText, Paint paint) {
         Rect bounds = new Rect();
-        getTextPaint().getTextBounds(targetText, 0, targetText.length(), bounds);
-        float width =bounds.right-bounds.left;
-        toPoint.x= (int) (toPoint.x-width/2);
-        drawTextOnMiddle(canvas,targetText,toPoint.x,toPoint.y);
+        paint.getTextBounds(targetText, 0, targetText.length(), bounds);
+        float width = bounds.right - bounds.left;
+        toPoint.x = (int) (toPoint.x - width / 2);
+        drawTextOnMiddle(canvas, targetText, toPoint.x, toPoint.y, paint);
 
     }
 
     private void drawTextOnMiddle(Canvas canvas, String targetText, float x, float y) {
-        drawTextOnMiddle(canvas,targetText,x,y,getTextPaint());
+        drawTextOnMiddle(canvas, targetText, x, y, getTextPaint());
     }
+
     /**
      * 一个帮助你把文字写在指定点并居住在指定点位置的方法
      * 这里的偏移直角坐标系采用默认指教坐标系
@@ -200,8 +255,8 @@ public class ClockView extends View {
         /*文字测量*/
         Rect bounds = new Rect();
         paint.getTextBounds(targetText, 0, targetText.length(), bounds);
-        float width =bounds.right-bounds.left;
-        float height=bounds.bottom-bounds.top;
+        float width = bounds.right - bounds.left;
+        float height = bounds.bottom - bounds.top;
         canvas.drawText(targetText, x - width / 2, y + height / 2, paint);
     }
 
@@ -241,5 +296,139 @@ public class ClockView extends View {
             return point;
         }
     }
+
+
+    private int secondRepeat = 0;
+    double secondInitAngle = 0;
+    private int minuteRepeat = 0;
+    double minuteInitAngle = 0;
+    private int hourRepeat = 0;
+    double hourInitAngle = 0;
+    private int dayHalfRepeat = 0;
+    double dayHalfInitAngle = 0;
+    private int dayRepeat = 0;
+    double dayInitAngle = 0;
+    private int monthRepeat = 0;
+    double monInitAngle = 0;
+
+     ValueAnimator last;
+    public void startAnimator() {
+        if (last!=null){
+            last.cancel();
+        }
+        final ValueAnimator valueAnimator= ValueAnimator.ofFloat(0, 1);
+        last=valueAnimator;
+
+        /***
+         * 秒针动画
+         * */
+
+        valueAnimator.setRepeatMode(ValueAnimator.RESTART);
+        valueAnimator.setDuration(1000);
+        valueAnimator.setRepeatCount(59);
+        valueAnimator.setInterpolator(new LinearInterpolator());
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                secondInitAngle = ((float) animation.getAnimatedValue() * 360.0 / 60);
+                if (secondRepeat == 59) {
+                    minuteInitAngle = secondInitAngle;
+                    if (minuteRepeat == 59) {
+                        hourInitAngle = ((float) animation.getAnimatedValue() * 360.0 / 12);
+                        if (hourRepeat == 11) {
+                            dayHalfInitAngle = ((float) animation.getAnimatedValue() * 360.0 / 2);
+                            int dayLength=CommonNumber.getDays(null).length;
+                            if (dayHalfRepeat == 1) {
+                                dayInitAngle = ((float) animation.getAnimatedValue() * 360.0 /dayLength);
+                                if (dayRepeat==1){
+                                    monInitAngle=((float) animation.getAnimatedValue() * 360.0 / 12);
+                                }
+                            }
+
+                        }
+
+                    }
+
+                }
+
+                invalidate();
+
+            }
+
+        });
+
+        valueAnimator.addListener(new AnimatorListenerAdapter() {
+
+            int secondTemp = 0;
+            int minuteTemp = 0;
+            int hourTemp = 0;
+            int dayHalfTemp = 0;
+            int dayTemp = 0;
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+                secondRepeat++;
+                if (secondRepeat == 1) {
+                    minuteRepeat += secondTemp;
+                    secondTemp = 0;
+                    minuteInitAngle = 0;
+
+                    hourRepeat += minuteTemp;
+                    minuteTemp = 0;
+                    hourInitAngle = 0;
+
+                    dayHalfRepeat += hourTemp;
+                    hourTemp = 0;
+                    dayHalfInitAngle = 0;
+
+                    dayRepeat += dayHalfTemp;
+                    dayHalfTemp = 0;
+                    dayInitAngle = 0;
+
+                    monthRepeat += dayTemp;
+                    dayTemp = 0;
+                    monInitAngle = 0;
+                }
+                super.onAnimationRepeat(animation);
+
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+                super.onAnimationCancel(animation);
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                //  isMove=false;
+                //  invalidate();
+                secondRepeat++;
+                valueAnimator.start();
+                secondTemp = 1;
+                secondRepeat = secondRepeat % 60;
+                minuteRepeat = minuteRepeat % 60;
+                hourRepeat = hourRepeat % 12;
+                dayHalfRepeat = dayHalfRepeat % 2;
+                int dayLength=CommonNumber.getDays(null).length;
+                dayRepeat=dayRepeat%dayLength;
+                monthRepeat=monthRepeat%12;
+
+
+                super.onAnimationEnd(animation);
+            }
+
+            @Override
+            public void onAnimationStart(Animator animation) {
+                isMove = true;
+                super.onAnimationStart(animation);
+
+            }
+
+        });
+        valueAnimator.start();
+
+
+    }
+
 
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,19 +4,65 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+    ClockView clockView;
+    EditText et_month;
+    EditText et_day;
+    EditText et_hour;
+    EditText et_minute;
+    EditText et_second;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        clockView = findViewById(R.id.clock_view);
+        et_month = findViewById(R.id.et_month);
+        et_day = findViewById(R.id.et_day);
+        et_hour = findViewById(R.id.et_hour);
+        et_minute = findViewById(R.id.et_minute);
+        et_second = findViewById(R.id.et_second);
+        findViewById(R.id.btn_send).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (
+                        TextUtils.isEmpty(et_month.getText()) ||
+                                TextUtils.isEmpty(et_day.getText()) ||
+                                TextUtils.isEmpty(et_hour.getText()) ||
+                                TextUtils.isEmpty(et_minute.getText()) ||
+                                TextUtils.isEmpty(et_second.getText())
+
+                ) {
+                    Toast.makeText(MainActivity.this, "不能为空", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                int month=Integer.parseInt(et_month.getText().toString());
+                int day=Integer.parseInt(et_day.getText().toString());
+                int hour=Integer.parseInt(et_hour.getText().toString());
+                int minute=Integer.parseInt(et_minute.getText().toString());
+                int second=Integer.parseInt(et_second.getText().toString());
+                Calendar calendar=Calendar.getInstance();
+                calendar.set(Calendar.MONTH,month);
+                calendar.set(Calendar.DAY_OF_MONTH,day);
+                calendar.set(Calendar.HOUR_OF_DAY,hour);
+                calendar.set(Calendar.MINUTE,minute);
+                calendar.set(Calendar.SECOND,second);
+                clockView.setDateAndStart(calendar);
+
+            }
+        });
         findViewById(R.id.btn_line_fill).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent it=new Intent();
-                it.setClass(MainActivity.this,LineFillActivity.class);
+                Intent it = new Intent();
+                it.setClass(MainActivity.this, LineFillActivity.class);
                 startActivity(it);
 
             }
